fcb31cddc534edebbca734b4f4337e74d574766f
Augustin Anders <121796762+augustinanders@users.noreply.github.com>
2023-03-09 20:27:42 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import Card from "./components/Card";
+
+export default function App() {
+  const fruits = [
+    {
+      id: 1337,
+      name: "🍌 Banana",
+      color: "yellow",
+    },
+    {
+      id: 1338,
+      name: "🍎 Apple",
+      color: "red",
+    },
+    {
+      id: 1339,
+      name: "🍊 Orange",
+      color: "orange",
+    },
+  ];
+
+  return (
+    <div className="app">
+      {fruits.map(({ id, name, color }) => (
+        <Card key={id} name={name} color={color} />
+      ))}
+    </div>
+  );
+}
